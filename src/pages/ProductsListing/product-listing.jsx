3495679cf701d932c45561aa734c207a56ef7a39
Devananth <devananthnkl@gmail.com>
2022-03-21 13:56:15 +0530

    Add : created basic products page

---
+++ src/pages/ProductsListing/product-listing.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../../components";
+
+const ProductListing = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>Product Listing</h1>
+    </>
+  );
+};
+
+export { ProductListing };
