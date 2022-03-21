958aaa761d56a72a93f21709ffe6306a728337a0
Devananth <devananthnkl@gmail.com>
2022-03-21 13:53:42 +0530

    Add : added markup and styles for Footer component

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="d-flex xy-center">
+      <p>Made with ❤ by Devananth © 2022</p>
+    </footer>
+  );
+};
+
+export { Footer };
