562354394db179a73905f402995fbbda999e6bc8
Devananth <devananthnkl@gmail.com>
2022-04-07 03:17:49 +0530

    Feat : created cart page

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,10 +1,33 @@
-import { NavBar } from "../../components";
+import { Link } from "react-router-dom";
+import { NavBar, CartCard, CartPriceCard } from "../../components";
+import { useCart } from "../../contexts";
+import { CartContainer } from "./CartContainer";
 
 const Cart = () => {
+  const {
+    cartState: { cart },
+    cartDispatch,
+  } = useCart();
+
   return (
     <>
       <NavBar />
-      <h1>Cart </h1>
+      <h4 className="txt-bold txt-center my-2">My Cart {`(${cart.length})`}</h4>
+      <section className="cart__wrapper d-flex">
+        {cart.length ? (
+          <>
+            <CartContainer />
+            <CartPriceCard />
+          </>
+        ) : (
+          <h3 className="txt-bold txt-center w-100">
+            No items in cart
+            <Link to={"/products"} className="btn btn-primary m-1">
+              Shop Now
+            </Link>
+          </h3>
+        )}
+      </section>
     </>
   );
 };
