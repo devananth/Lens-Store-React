8e64d8f24b1ba448b8eb55f2e8aed83922ef09d6
Devananth <devananthnkl@gmail.com>
2022-04-06 10:34:16 +0530

    Feat : added Login , Signup pages

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,11 +2,7 @@ import { Link } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 import { NavBar, FormContainer, FormInput, FormButton } from "../../components";
 
-const Login = () => {
-  const [loginForm, setLoginForm] = useState({ email: "", password: "" });
-
-  const loginHandler = () => {};
-
+const SignUp = () => {
   return (
     <>
       <NavBar />
@@ -14,17 +10,30 @@ const Login = () => {
         className="d-flex xy-center"
         style={{ minHeight: "calc(100vh - 4rem)" }}
       >
-        <FormContainer formHeadingText={"Login"} submitHandler={() => true}>
+        <FormContainer formHeadingText={"SignUp"} submitHandler={() => true}>
+          <FormInput
+            label={"First Name"}
+            type={"text"}
+            placeholder={"John"}
+            name={"firstName"}
+            id={uuid()}
+            inputHandler={() => true}
+          />
+          <FormInput
+            label={"Last Name"}
+            type={"text"}
+            placeholder={"Doe"}
+            name={"lastName"}
+            id={uuid()}
+            inputHandler={() => true}
+          />
           <FormInput
             label={"Email Address"}
             type={"email"}
             placeholder={"john@gemail.com"}
             name={"EmailAddress"}
             id={uuid()}
-            value={loginForm.email}
-            inputHandler={(event) =>
-              setLoginForm({ ...loginForm, email: event.target.value })
-            }
+            inputHandler={() => true}
           />
           <FormInput
             label={"Password"}
@@ -32,33 +41,30 @@ const Login = () => {
             placeholder={"********"}
             name={"password"}
             id={uuid()}
-            value={loginForm.password}
-            inputHandler={(event) =>
-              setLoginForm({ ...loginForm, password: event.target.value })
-            }
+            inputHandler={() => true}
           />
 
           <div className="d-flex space-bw wrap">
             <div className="form-group width-content d-flex y-center gap-1">
-              <input type="checkbox" className="form-input" id="rememberMe" />
+              <input type="checkbox" className="form-input" id="declaration" />
               <label
                 className="form-label txt-sm d-inline-block"
-                htmlFor="rememberMe"
+                htmlFor="declaration"
               >
-                Remember Me
+                I accept all Terms and Conditions
               </label>
             </div>
-            <Link to={"#"} className="mb-1 txt-sm d-inline-block txt-primary">
-              Forgot your password?
-            </Link>
           </div>
 
-          <FormButton btnText={"Login"} btnClickHandler={() => true} />
+          <FormButton
+            btnText={"Create New Account"}
+            btnClickHandler={() => true}
+          />
 
           <div>
-            <span>Not a user yet ?</span>
-            <Link to={"/signup"} className="btn btn-link btn-link-primary">
-              Create New Account
+            <span>Already have an account ?</span>
+            <Link to={"/login"} className="btn btn-link btn-link-primary">
+              Login Here
             </Link>
           </div>
         </FormContainer>
@@ -67,4 +73,4 @@ const Login = () => {
   );
 };
 
-export { Login };
+export { SignUp };
