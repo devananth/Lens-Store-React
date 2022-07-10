4e454993b0fb1926ef8ad61daa91098812aba534
Devananth <devananthnkl@gmail.com>
2022-07-10 12:51:30 +0530

    feat : added api calls to wishlist context

--- src/contexts/wishlist-context.js
+++ src/contexts/wishlist-context.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useReducer, useEffect } from "react";
-import { wishlistReducer } from "../reducers";
+import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useAxios } from "../custom-hooks";
+import { useAuth } from "./auth-context";
 
 const WishListContext = createContext({});
 
@@ -8,14 +10,63 @@ const initialwishlistState = {
 };
 
 const WishlistProvider = ({ children }) => {
+  const [wishlist, setWishlist] = useState([]);
+
+  const {
+    authState: { isUserLoggedIn, authToken },
+  } = useAuth();
+
+  const { loader, response, error, callAPI } = useAxios();
+
+  useEffect(() => {
+    if (isUserLoggedIn) {
+      callAPI({
+        url: "/api/user/wishlist",
+        method: "get",
+        headers: { authorization: authToken },
+      });
+    }
+  }, [isUserLoggedIn]);
+
+  useEffect(() => {
+    if (response) {
+      const updatedList = response?.data?.wishlist;
+
+      if (updatedList.length > wishlist.length) {
+        toast.success("Added to wishlist");
+      } else if (updatedList.length < wishlist.length) {
+        toast.success("Removed from wishlist");
+      }
+      setWishlist(updatedList);
+    }
+  }, [response]);
+
+  const addToWishlistServerCall = (product) => {
+    console.log("Add wishlist : ", product);
+    callAPI({
+      url: "/api/user/wishlist",
+      method: "post",
+      headers: { authorization: authToken },
+      data: { product },
+    });
+  };
+
+  const removeFromWishlistServerCall = (product) => {
+    callAPI({
+      url: `/api/user/wishlist/${product._id}`,
+      method: "delete",
+      headers: { authorization: authToken },
+    });
+  };
+
   const { Provider } = WishListContext;
 
-  const [wishlistState, wishlistDispatch] = useReducer(
-    wishlistReducer,
-    initialwishlistState
-  );
-
-  const value = { wishlistState, wishlistDispatch };
+  const value = {
+    wishlist,
+    setWishlist,
+    addToWishlistServerCall,
+    removeFromWishlistServerCall,
+  };
 
   return <Provider value={value}>{children}</Provider>;
 };
